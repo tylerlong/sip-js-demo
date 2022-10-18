ca1e35bb35c48d50595b768c3935decc20daf0dd
Tyler Long <tyler4long@gmail.com>
2022-10-18 16:18:26 -0700

    Login RC

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './index.css';
 
+import App from './main';
+
 const container = document.createElement('div');
 document.body.appendChild(container);
 
 const root = ReactDOM.createRoot(container);
-root.render(<h1>Hello world!</h1>);
+root.render(<App />);
---
+++ src/main.tsx
@@ -0,0 +1,158 @@
+import 'antd/dist/antd.css';
+import React, {useEffect, useState} from 'react';
+import {Button, Form, Input, message, Select} from 'antd';
+import localforage from 'localforage';
+import RingCentral from '@rc-ex/core';
+
+class LoginForm {
+  serverUrl!: string;
+  clientId!: string;
+  clientSecret!: string;
+  username!: string;
+  extension?: string;
+  password!: string;
+}
+
+let rc: RingCentral;
+let loginForm: LoginForm;
+const remoteVideoElement = document.getElementById(
+  'remote-video'
+) as HTMLVideoElement;
+const localVideoElement = document.getElementById(
+  'local-video'
+) as HTMLVideoElement;
+
+const App = () => {
+  const login = async (_loginForm: LoginForm) => {
+    loginForm = _loginForm;
+    await localforage.setItem('wp-login-form', loginForm);
+    rc = new RingCentral({
+      server: loginForm.serverUrl,
+      clientId: loginForm.clientId,
+      clientSecret: loginForm.clientSecret,
+    });
+    await rc.authorize({
+      username: loginForm.username,
+      extension: loginForm.extension,
+      password: loginForm.password,
+    });
+    message.success('You have logged in!');
+    setLoggedIn(true);
+    await postLogin();
+  };
+
+  const logout = () => {
+    setLoggedIn(false);
+    if (rc !== undefined) {
+      rc.revoke();
+    }
+    message.success('You have logged out!');
+  };
+
+  const postLogin = async () => {
+    const sipInfo = await rc
+      .restapi()
+      .clientInfo()
+      .sipProvision()
+      .post({
+        sipInfo: [
+          {
+            transport: 'WSS',
+          },
+        ],
+      });
+  };
+
+  const [form] = Form.useForm();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const loginForm = await localforage.getItem('wp-login-form');
+      form.setFieldsValue(loginForm);
+    })();
+  });
+
+  return (
+    <>
+      <h1>RingCentral Web Phone Demo</h1>
+      {loggedIn ? (
+        <>
+          <video id="remote-video" hidden></video>
+          <video id="local-video" hidden muted></video>
+          <Button onClick={logout}>Log out</Button>
+        </>
+      ) : (
+        <Form
+          name="basic"
+          labelCol={{span: 8}}
+          wrapperCol={{span: 8}}
+          onFinish={login}
+          autoComplete="off"
+          form={form}
+        >
+          <Form.Item
+            label="Server URL"
+            name="serverUrl"
+            rules={[{required: true, message: 'Please input the Server URL!'}]}
+          >
+            <Select>
+              <Select.Option value="https://platform.ringcentral.com">
+                https://platform.ringcentral.com
+              </Select.Option>
+              <Select.Option value="https://platform.devtest.ringcentral.com">
+                https://platform.devtest.ringcentral.com
+              </Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item
+            label="Client Id"
+            name="clientId"
+            rules={[{required: true, message: 'Please input the Client Id!'}]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Client Secret"
+            name="clientSecret"
+            rules={[
+              {required: true, message: 'Please input the Client Secret!'},
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{required: true, message: 'Please input the Username!'}]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Extension" name="extension">
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{required: true, message: 'Please input the Password!'}]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{offset: 8, span: 8}}>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      )}
+    </>
+  );
+};
+
+export default App;
